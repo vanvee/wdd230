9280e1cfe312962a93c4e1b7b1eaca0c586538ce
vanvee <92326739+vanvee@users.noreply.github.com>
2022-01-09 12:18:38 +0300

    my js

---
+++ images/js/dates.js
@@ -0,0 +1,43 @@
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+// using getElementById
+document.getElementById("currentdate").textContent = fulldate;
+// ****************************
+// using querySelector
+const date2 = document.querySelector('#currentdate2');
+
+try {
+  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  date2.textContent = new Date().toLocaleDateString('en-UK', options);
+} catch (e) {
+  alert('Error with code or your browser does not support Locale');
+}
+ 
